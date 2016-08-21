cca3117bfd8f805332a9523919b2625253417d0c
Catherine Lankiewicz <cathylankie@gmail.com>
2016-08-21 14:54:03 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-var new = "vanilla';
+var validationForm = function(){
+
+    var name = document.getElementById("userName").value;
+    var number = document.getElementById("userNumber").value;
+
+    name = name.toLowerCase();
+
+    console.log(name + " " +number);
+
+};
+
+var request = new XMLHttpRequest();
