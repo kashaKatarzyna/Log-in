f892731083311f1dbca99f85d7b0c905ea1fe41d
Catherine Lankiewicz <cathylankie@gmail.com>
2016-08-25 12:01:07 +0300

    Add files via upload

---
+++ dashboard.js
@@ -0,0 +1,4 @@
+// ZmJhYzczNDgtZjBkNy00ZjI2LWExZjEtOGFiYjRjNzU3MTcw
+
+// redirect and pass token to dashboard page and save in local storage
+// create table to display each document type. have a side nav with 3 options, when user clicks on each it directs them to the documents
